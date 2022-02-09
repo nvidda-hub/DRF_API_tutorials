58c1255a1add126c9e07819ec998c57cbac62f08
nvidda-hub <nkvidda@gmail.com>
2022-02-09 23:55:15 +0530

    Added basic authenitcation for single class or Not globally

--- API_1/urls.py
+++ API_1/urls.py
@@ -13,10 +13,6 @@ router.register('students', views.StudentModelViewSet, basename='students')
 
 
 urlpatterns = [
-    # path('students/', views.AllStudentsView.as_view()),
-    # path('students/<int:pk>/', views.StudentDetailAPIView.as_view()),
     path('viewset/', include(router.urls)),
     path('viewset/<int:pk>/', include(router.urls)),
-    # path('stu/', views.get_student),
-    # path('stu/', views.StudentView.as_view()),
 ]

e5b66d20f385550d2175e8afe392bedad6564aff
nvidda-hub <nkvidda@gmail.com>
2022-02-10 00:47:06 +0530

    starting session authentication

--- API_1/views.py
+++ API_1/views.py
@@ -1,22 +1,36 @@
 from API_1.models import Student
 from API_1.serializers import StudentSerializer
 from rest_framework import viewsets
-from rest_framework.authentication import BasicAuthentication
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
 
-class StudentModelViewSet(viewsets.ModelViewSet):
-    # gives out permissions this way if we have few classes to give permission
-    # other wise define globally means writeout in settings.py
-    # authentication_classes = [BasicAuthentication]
-    # permission_classes = [IsAuthenticated]
-    serializer_class = StudentSerializer
-    queryset = Student.objects.all()
 
-class StudentModelViewSetOveridden(viewsets.ModelViewSet):
-    # gives out permissions this way if we have few classes to give permission
-    # other wise define globally means writeout in settings.py
-    # authentication_classes = [BasicAuthentication]
-    permission_classes = [AllowAny]
+# For Basic Authentication
+
+
+# class StudentModelViewSet(viewsets.ModelViewSet):
+#     # gives out permissions this way if we have few classes to give permission
+#     # other wise define globally means writeout in settings.py
+#     # authentication_classes = [BasicAuthentication]
+#     # permission_classes = [IsAuthenticated]
+#     serializer_class = StudentSerializer
+#     queryset = Student.objects.all()
+
+# class StudentModelViewSetOveridden(viewsets.ModelViewSet):
+#     # gives out permissions this way if we have few classes to give permission
+#     # other wise define globally means writeout in settings.py
+#     # authentication_classes = [BasicAuthentication]
+#     permission_classes = [AllowAny]
+#     serializer_class = StudentSerializer
+#     queryset = Student.objects.all()
+
+
+
+# sesssion authentication
+
+class StudentModelViewSet(viewsets.ModelViewSet):
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated]
     serializer_class = StudentSerializer
     queryset = Student.objects.all()

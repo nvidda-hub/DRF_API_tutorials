16c18b3940fadb41484eca5ca78c955dbdcb58a4
nvidda-hub <nkvidda@gmail.com>
2022-02-10 09:03:12 +0530

    custom auth token working

--- API_1/urls.py
+++ API_1/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from API_1 import views
 from rest_framework.routers import DefaultRouter
-from rest_framework.authtoken.views import obtain_auth_token
+# from rest_framework.authtoken.views import obtain_auth_token
+from API_1.custom_token import CustomAuthToken
 
 
 router = DefaultRouter()
@@ -18,6 +19,8 @@ urlpatterns = [
     path('viewset/', include(router.urls)),
     path('viewset/<int:pk>/', include(router.urls)),
     # path('auth/', include('rest_framework.urls', namespace='sessionAuth')),  
-    path('gettoken/', obtain_auth_token),  
+    path('gettoken/', CustomAuthToken.as_view()),  
     # to provide credentials for session auth b'coz unlike basic auth there is no promopt
 ]
+
+# auth token use ->  http POST http://127.0.0.1:8000/api1/v1/gettoken/ username="user2" password="userpassword2"

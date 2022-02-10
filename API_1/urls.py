36a56961e6d2f7e56a9f0bee4eda6d3df41d4d22
nvidda-hub <nkvidda@gmail.com>
2022-02-10 14:55:47 +0530

    simple jwt generating, verifying and refreshing tokens

--- API_1/urls.py
+++ API_1/urls.py
@@ -2,7 +2,8 @@ from django.urls import path, include
 from API_1 import views
 from rest_framework.routers import DefaultRouter
 # from rest_framework.authtoken.views import obtain_auth_token
-from API_1.custom_token import CustomAuthToken
+# from API_1.custom_token import CustomAuthToken
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 
 router = DefaultRouter()
@@ -19,7 +20,10 @@ urlpatterns = [
     path('viewset/', include(router.urls)),
     path('viewset/<int:pk>/', include(router.urls)),
     # path('auth/', include('rest_framework.urls', namespace='sessionAuth')),  
-    path('gettoken/', CustomAuthToken.as_view()),  
+    # path('gettoken/', CustomAuthToken.as_view()),  
+    path('get/token/', TokenObtainPairView.as_view(), name='token_auth_pair'),  
+    path('refresh/token/', TokenRefreshView.as_view(), name='token_refresh'),  
+    path('verify/token/', TokenVerifyView.as_view(), name='token_verify'),  
     # to provide credentials for session auth b'coz unlike basic auth there is no promopt
 ]
 
--- DevTest/settings.py
+++ DevTest/settings.py
@@ -22,7 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken',
+    # 'rest_framework.authtoken',
     'API_1',
 ]
 

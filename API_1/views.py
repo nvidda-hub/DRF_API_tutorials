3798b593d4f56cffc9cba2b95d89573a91333d5f
nvidda-hub <nkvidda@gmail.com>
2022-02-10 15:36:23 +0530

    simple jwt implemented

--- API_1/views.py
+++ API_1/views.py
@@ -3,35 +3,14 @@ from API_1.serializers import StudentSerializer
 from rest_framework import viewsets
 from rest_framework.authentication import BasicAuthentication, SessionAuthentication, TokenAuthentication
 from API_1.custom_auth import CustomAuthentication
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from API_1.custom_permissions import MyPermission
 
 
-# For Basic Authentication
-
-
-# class StudentModelViewSet(viewsets.ModelViewSet):
-#     # gives out permissions this way if we have few classes to give permission
-#     # other wise define globally means writeout in settings.py
-#     # authentication_classes = [BasicAuthentication]
-#     # permission_classes = [IsAuthenticated]
-#     serializer_class = StudentSerializer
-#     queryset = Student.objects.all()
-
-# class StudentModelViewSetOveridden(viewsets.ModelViewSet):
-#     # gives out permissions this way if we have few classes to give permission
-#     # other wise define globally means writeout in settings.py
-#     # authentication_classes = [BasicAuthentication]
-#     permission_classes = [AllowAny]
-#     serializer_class = StudentSerializer
-#     queryset = Student.objects.all()
-
-
-
-# sesssion authentication
 
 class StudentModelViewSet(viewsets.ModelViewSet):
-    authentication_classes = [CustomAuthentication]
+    authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]      # http://127.0.0.1:8000/api1/v1/viewset/students/?username=user3
     serializer_class = StudentSerializer
     queryset = Student.objects.all()

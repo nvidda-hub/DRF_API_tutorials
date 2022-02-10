2c1c866aa0b435135dc9d22297910c4464e4ba04
nvidda-hub <nkvidda@gmail.com>
2022-02-10 14:18:30 +0530

    custom authentication done and starting simple JWT

--- API_1/views.py
+++ API_1/views.py
@@ -2,6 +2,7 @@ from API_1.models import Student
 from API_1.serializers import StudentSerializer
 from rest_framework import viewsets
 from rest_framework.authentication import BasicAuthentication, SessionAuthentication, TokenAuthentication
+from API_1.custom_auth import CustomAuthentication
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from API_1.custom_permissions import MyPermission
 
@@ -30,7 +31,7 @@ from API_1.custom_permissions import MyPermission
 # sesssion authentication
 
 class StudentModelViewSet(viewsets.ModelViewSet):
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    authentication_classes = [CustomAuthentication]
+    permission_classes = [IsAuthenticated]      # http://127.0.0.1:8000/api1/v1/viewset/students/?username=user3
     serializer_class = StudentSerializer
     queryset = Student.objects.all()
